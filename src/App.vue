c1e3dd92a8efc25d834b24ffbda509f5c70ebd3b
mohamed youssef chlendi <youssef.chlendi@gmail.com>
2021-12-31 22:39:47 +0100

    d

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
   <div id="nav">
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link> |
+    <router-link to="/etudient">Etudient</router-link> |
     <router-link to="/class">Class</router-link>
   </div>
   <router-view />
