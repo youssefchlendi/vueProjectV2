5c40671f5c83254c3a746981a1bc7d2ff8f349a1
mohamed youssef chlendi <youssef.chlendi@gmail.com>
2021-12-31 22:39:42 +0100

    add class route

--- src/router/index.js
+++ src/router/index.js
@@ -25,6 +25,15 @@ const routes = [
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/Class.vue"),
   },
+  {
+    path: "/etudient",
+    name: "Etudient",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/Etudient.vue"),
+  },
 ];
 
 const router = createRouter({
