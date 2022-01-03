b45418b0dd2ff0a3bf3b4da4d16f7d72a6e09470
mohamed youssef chlendi <youssef.chlendi@gmail.com>
2022-01-03 01:18:17 +0100

    add search into router

--- src/router/index.js
+++ src/router/index.js
@@ -61,6 +61,15 @@ const routes = [
     // which is lazy-loaded when the route is visited.
     component: Examen,
   },
+  {
+    path: "/search",
+    name: "Search",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/Search.vue"),
+  },
 ];
 
 const router = createRouter({
